b162ced5d56a02730be31e8b4bd50ce823c6ebe1
Artyom <goglakk2@gmail.com>
2025-06-27 17:58:15 +0300

    Adapter

---
+++ app/src/main/java/com/example/photogallery/ImageAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.photogallery
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.photogallery.databinding.ItemImageBinding
+import com.example.photogallery.models.PhotoResult
+
+class ImageAdapter(private val images: List<PhotoResult>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(view: View) : ViewHolder(view) {
+        private val binding = ItemImageBinding.bind(view)
+
+        val imageView: ImageView = binding.imageView
+        val titleView: TextView = binding.titleTV
+        val licenceView: TextView = binding.licenceTV
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun getItemCount() = images.size
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val image = images[position]
+        Glide.with(holder.imageView.context).load(image.url).into(holder.imageView)
+
+        holder.titleView.text = image.title
+        holder.licenceView.text = "Licence: ${image.license}"
+    }
+
+
+}
--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.photogallery
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.photogallery.databinding.ActivityMainBinding
+import com.example.photogallery.models.PhotoResult
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adapter: ImageAdapter
+    private val images = mutableListOf<PhotoResult>()
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,21 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
-        
+        binding.RV.layoutManager = GridLayoutManager(this, 2)
+        adapter = ImageAdapter(images)
+        binding.RV.adapter = adapter
+
+        binding.Btn.setOnClickListener {
+            val query = binding.EditText.text.toString()
+            if (query.isNotEmpty()) {
+                searchImages(query)
+            }
+            else{
+                Toast.makeText(this, "Enter search query", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun searchImages(query: String){
     }
 }
