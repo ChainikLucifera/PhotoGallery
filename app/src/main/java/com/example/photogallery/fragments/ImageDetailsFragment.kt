ae73e6b5023b899dd7a940e47764fdf6881cd817
Artyom <goglakk2@gmail.com>
2026-02-12 19:59:24 +0200

    Details fragment favourite button added

--- app/src/main/java/com/example/photogallery/fragments/ImageDetailsFragment.kt
+++ app/src/main/java/com/example/photogallery/fragments/ImageDetailsFragment.kt
@@ -1,13 +1,15 @@
 package com.example.photogallery.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.photogallery.databinding.FragmentImageDetailsBinding
 import com.example.photogallery.models.Result
+import com.example.photogallery.utils.FavouritesManager
 
 
 class ImageDetailsFragment : Fragment() {
@@ -25,6 +27,7 @@ class ImageDetailsFragment : Fragment() {
         binding = FragmentImageDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -32,16 +35,40 @@ class ImageDetailsFragment : Fragment() {
             .load(image.url)
             .into(binding.detailImageView)
 
-        with(binding){
+        with(binding) {
             titleTextView.text = image.title
             creatorTextView.text = "Creator: ${image.creator ?: "Unknown"}"
             licenceTextView.text = "Licence: ${image.license} ${image.license_version ?: ""}"
             dimensionsTextView.text = "Dimensions: ${image.width ?: "?"} x ${image.height ?: "?"}"
             sourceTextView.text = "Source: ${image.source ?: "Unknown"}"
-            closeButton.setOnClickListener{
+            closeButton.setOnClickListener {
                 parentFragmentManager.popBackStack()
             }
+
+            updateFavouriteButton()
+
+            favouriteButton.setOnClickListener {
+                val isCurrentlyFavourite = FavouritesManager.isFavorite(image.id)
+                if (isCurrentlyFavourite) {
+                    FavouritesManager.removeFromFavourites(image.id)
+                    Toast.makeText(requireContext(), "Removed From Favourites", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    FavouritesManager.addToFavourites(image)
+                    Toast.makeText(requireContext(), "Added to favourites", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                updateFavouriteButton()
+            }
         }
+    }
+
+    fun updateFavouriteButton() {
+        if (FavouritesManager.isFavorite(image.id))
+            binding.favouriteButton.setImageResource(android.R.drawable.btn_star_big_on)
+        else
+            binding.favouriteButton.setImageResource(android.R.drawable.btn_star_big_off)
     }
 
     companion object {
